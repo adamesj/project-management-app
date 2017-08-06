6e4e551353041ff28a6d56be5e2889214a718aa5
Jimmy Adames <jimmy.adames@gmail.com>
2017-08-06 18:10:41 -0400

    Configuration for mailers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require bootstrap
+//= require jquery
+//= require jquery_ujs
 //= require tether
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mailer options
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'jadames-project-manager.herokuapp.com', :protocol => 'https' }
 end
